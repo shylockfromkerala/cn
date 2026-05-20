b71f0dfda418753803274c4b42a5d6382e0fd8db
shylockfromkerala <shylockismerchant@gmail.com>
2026-05-20 18:12:45 +0530

    Update server.c

--- cnlab/palindrome/server.c
+++ cnlab/palindrome/server.c
@@ -6,90 +6,83 @@
 
 int main()
 {
-    // Socket descriptors
-    int server_fd, client_fd;
+    int sockfd, newSocket;
 
-    // Structures to store server and client address
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in serverAddr, clientAddr;
 
-    // Variable to store client address size
-    socklen_t len = sizeof(client_addr);
+    socklen_t addr_size;
 
-    // Buffer to store received data
-    char buffer[1024];
+    char str[100];
 
-    // Create TCP socket
-    // AF_INET -> IPv4
-    // SOCK_STREAM -> TCP
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    char result[100];
 
-    // Check socket creation
-    if(server_fd < 0)
+    int flag = 1;
+
+    // Create socket
+    sockfd = socket(AF_INET,
+                    SOCK_STREAM,
+                    0);
+
+    // Server details
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5000);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind
+    bind(sockfd,
+         (struct sockaddr*)&serverAddr,
+         sizeof(serverAddr));
+
+    // Listen
+    listen(sockfd, 5);
+
+    printf("Server Waiting...\n");
+
+    addr_size = sizeof(clientAddr);
+
+    // Accept connection
+    newSocket = accept(sockfd,
+                       (struct sockaddr*)&clientAddr,
+                       &addr_size);
+
+    // Receive string
+    recv(newSocket,
+         str,
+         sizeof(str),
+         0);
+
+    printf("String Received : %s\n", str);
+
+    int len = strlen(str);
+
+    // Check palindrome
+    for(int i = 0; i < len / 2; i++)
     {
-        printf("Socket creation failed\n");
-        return 0;
-    }
-
-    // Set IPv4 address family
-    server_addr.sin_family = AF_INET;
-
-    // Set port number = 8080
-    server_addr.sin_port = htons(8080);
-
-    // Accept connection from any IP address
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    // Bind socket with IP and port
-    bind(server_fd,
-         (struct sockaddr*)&server_addr,
-         sizeof(server_addr));
-
-    // Listen for incoming clients
-    // 5 = maximum waiting clients
-    listen(server_fd, 5);
-
-    printf("Waiting for client...\n");
-
-    // Accept client connection
-    // Creates new communication socket
-    client_fd = accept(server_fd,
-                      (struct sockaddr*)&client_addr,
-                      &len);
-
-    // Infinite loop for continuous communication
-    while(1)
-    {
-        // Receive frame from client
-        recv(client_fd,
-             buffer,
-             sizeof(buffer),
-             0);
-
-        // Check whether client wants to exit
-        if(strcmp(buffer, "exit") == 0)
+        if(str[i] != str[len - i - 1])
         {
-            printf("Client disconnected\n");
+            flag = 0;
             break;
         }
-
-        // Print received frame
-        printf("Received Frame: %s\n", buffer);
-
-        // Send acknowledgement to client
-        // This is STOP-AND-WAIT logic
-        send(client_fd,
-             "ACK",
-             3,
-             0);
-
-        printf("ACK Sent\n");
     }
 
-    // Close client socket
-    close(client_fd);
+    if(flag == 1)
+    {
+        strcpy(result, "Palindrome");
+    }
+    else
+    {
+        strcpy(result, "Not Palindrome");
+    }
 
-    // Close server socket
-    close(server_fd);
+    // Send result
+    send(newSocket,
+         result,
+         sizeof(result),
+         0);
+
+    close(newSocket);
+
+    close(sockfd);
 
     return 0;
 }
